310a5161fbe0c9c5e681e7deb7b5e32771c6dd80
YeDongVibe <wkwhstladmss@naver.com>
2023-05-08 17:55:06 +0900

    23.05.08 mission

--- sqlconnection/src/subject/test01.java
+++ sqlconnection/src/subject/test01.java
@@ -6,46 +6,52 @@ import java.util.Scanner;
 class QueryExe {
 	int num;
 	String text;
-	
-	QueryExe (int num, String text) {
+
+	QueryExe(int num, String text) {
 		this.num = num;
 		this.text = text;
 	}
-	
-	int getNum() { return num; }
-	String getText() { return text; }
+
+	int getNum() {
+		return num;
+	}
+
+	String getText() {
+		return text;
+	}
 }
 
 public class test01 {
-	
-	public static void exe01 () {
-		
+
+	public static void exe01() {
+
 	}
 
 	public static void main(String[] args) {
 
 		ArrayList<QueryExe> list = new ArrayList<>();
-		
+
 		list.add(new QueryExe(1, "London에 있는 프로젝트 이름을 찾아라"));
-		
+
 		Scanner sc = new Scanner(System.in);
-		while(true) {
-			
-			for(QueryExe qe : list) {
+		while (true) {
+
+			for (QueryExe qe : list) {
 				System.out.println(String.format("%d. %s", qe.getNum(), qe.getText()));
 			}
-			
+
 			System.out.println("선택 <0 : EXIT> : ");
 			int sel = sc.nextInt();
-			if (sel == 0) break;
-			
+			if (sel == 0)
+				break;
+
 			for (QueryExe qe : list) {
-				if(qe.getNum() == sel) {
-					
+				if (qe.getNum() == sel) {
+
 				}
 			}
 		}
-
+		sc.close();
 	}
 
 }
